7ca94b9cf956ac03bbc27ce1e2ddb3778f304e24
马和德 <2771551998@qq.com>
2018-12-19 17:55:12 +0800

    ruider

--- ruider_get/miniprogram/pages/index/index.js
+++ ruider_get/miniprogram/pages/index/index.js
@@ -11,9 +11,52 @@ Page({
   },
   //事件处理函数
   bindViewTap: function () {
-    wx.navigateTo({
-      url: '../tem/tem'    //'../user/userDetails'
+    wx.login({
+      success: function (res) {
+        var code = res.code;//登录凭证
+        if (code) {
+          //2、调用获取用户信息接口
+          wx.getUserInfo({
+            success: function (res) {
+              console.log({ encryptedData: res.encryptedData, iv: res.iv, code: code })
+              //3.请求自己的服务器，解密用户信息 获取unionId等加密信息
+              wx.request({
+                url: 'http://localhost:8080/userInfo/test',//自己的服务接口地址
+                method: 'get',
+                header: {
+                  "Content-Type": "applciation/json"
+                },
+                data: { encryptedData: res.encryptedData, iv: res.iv, code: code },
+                success: function (data) {
+                  console.log(data);
+                  //4.解密成功后 获取自己服务器返回的结果
+                  if (data.data.isSuccess) {
+                    var userInfo_ = data.data.data;
+                    console.log(userInfo_)
+                  } else {
+                    console.log('解密失败')
+                  }
+                },
+                fail: function () {
+                  console.log('系统错误')
+                }
+              })
+            },
+            fail: function () {
+              console.log('获取用户信息失败')
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + r.errMsg)
+        }
+      },
+      fail: function () {
+        console.log('登陆失败')
+      }
     })
+    /*wx.navigateTo({
+      url: '../tem/tem'    //'../user/userDetails'
+    })*/
   },
   
   bindMainPage: function (e) {
